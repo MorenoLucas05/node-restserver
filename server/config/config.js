9577ddb49a208e9dbe2e955a9441f0f4d17e7f77
Lucas <lucasmoreno891@gmail.com>
2020-07-21 21:34:03 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Gengar:bDXojqRrUWW8ucjr@cluster0.8fcyw.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
